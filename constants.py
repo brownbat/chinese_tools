1ca49920d75dcbae37bd4e0b2e7829d03b98b546
Thomas Brownback <brownbat@users.noreply.github.com>
2022-07-24 00:01:13 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,11 @@
+lowercase_vowels_with_tones = "āáǎàōóǒòēéěèīíǐìūúǔùǖǘǚǜ"
+uppercase_vowels_with_tones = "ĀÁǍÀŌÓǑÒĒÉĚÈĪÍǏÌŪÚǓÙǕǗǙǛ"
+vowels_with_tones = "āáǎàōóǒòēéěèīíǐìūúǔùǖǘǚǜĀÁǍÀŌÓǑÒĒÉĚÈĪÍǏÌŪÚǓÙǕǗǙǛ"
+
+initials = ["b", "p", "m", "f", "d", "t", "n", "l", "g", "k", "h", "j", "q", "x", "zh", "ch", "sh", "r", "z", "c", "s"]
+finals = [
+  "a", "o", "e", "ai", "ei", "ao", "ou", "an", "en", "ang", "eng", "er",  # Group a Finals
+  "i", "ia", "io", "ie", "iai", "iao", "iu", "ian", "in", "iang", "ing",  # Group i Finals
+  "u", "ua", "uo", "uai", "ui", "uan", "un", "uang", "ong",               # Group u Finals
+  "ü", "üe", "üan", "ün", "iong",                                         # Group ü Finals
+]
